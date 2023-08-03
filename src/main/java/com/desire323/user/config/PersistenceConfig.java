665b3db3812ac8372b44fcbecf44c820021e9381
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-08-03 16:31:45 +0300

    Add RestTemplate bean in PersistenceConfig

--- src/main/java/com/desire323/user/config/PersistenceConfig.java
+++ src/main/java/com/desire323/user/config/PersistenceConfig.java
@@ -3,12 +3,17 @@ package com.desire323.user.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfig {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
